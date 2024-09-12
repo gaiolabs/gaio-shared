f18664c49f8459e6319975e2ba5b1cfd237ae424
EstevanPithan <estevanpithan@hotmail.com>
2024-09-12 11:54:54 -0300

    feat: refactoring line chart

--- src/views/report/report-chart/ReportChartLine.vue
+++ src/views/report/report-chart/ReportChartLine.vue
@@ -18,86 +18,97 @@ import { onMounted, shallowRef } from 'vue'
 import useReportChartHelper from './ReportChartHelper'
 
 defineEmits(['change'])
-const props = defineProps<{ task: ReportNodeType; list: Record<string, unknown>[]; height: string }>()
+const { task, list, height } = defineProps<{ task: ReportNodeType; list: Record<string, unknown>[]; height: string }>()
 
-const chartHelper = computed(() => useReportChartHelper(props.task))
-const { dimensions, measures, settings, columnName, themeColors, label, firstMeasure, foundation } = chartHelper.value
+const chartHelper = computed(() => useReportChartHelper(task))
+const {
+	dimensions,
+	measures,
+	settings,
+	columnName,
+	themeColors,
+	label,
+	firstMeasure,
+	foundation,
+	isMultipleMeasure,
+	isGrouped
+} = chartHelper.value
 
 const { formatValue } = useFormatValue()
 const id = shallowRef()
 const chart = shallowRef()
 const localList = shallowRef([])
 
-const isMultipleMeasure = computed(() => {
-	return props.task.schema.select.filter((o) => o.type !== 'value').length > 1
-})
-
-const isGrouped = computed(() => {
-	return props.task.schema.select.filter((o) => o.type === 'value').length > 1
-})
-
-const loadChart = () => {
-	let common = {} as Partial<Record<string, unknown>>
-	if (!isGrouped.value && isMultipleMeasure.value) {
-		common.xField = columnName(dimensions.value[0])
-		common.yField = 'measure'
-		common.seriesField = 'category'
-	} else if (isGrouped.value) {
-		common.xField = columnName(dimensions.value[0])
-		common.yField = columnName(measures.value[0])
-		common.seriesField = columnName(dimensions.value[1])
-	} else {
-		common.xField = columnName(dimensions.value[0])
-		common.yField = columnName(measures.value[0])
-	}
-
-	chart.value = new Line(
-		id.value as HTMLElement,
-		{
-			data: localList.value,
-			...common,
-			...foundation.value,
-			lineStyle: {
-				lineWidth: settings.value.lineWidth
-			},
-			label: label({
-				formatter:
-					!settings.value.compactNumberLabel ?
-						null
-					:	(v) => {
-							const value = v['measure'] || v[columnName(firstMeasure.value)]
-							return formatValue(value, {
-								compactNumber: settings.value.compactNumberLabel
-							})
-						}
-			}),
-			smooth: settings.value.lineSmooth,
-			point:
-				settings.value.showPoint ?
-					{
-						shape: settings.value.pointType
-					}
-				:	null,
-			color:
-				isGrouped.value || isMultipleMeasure.value ? themeColors.value
-				: settings.value.showLegend ? themeColors.value
-				: themeColors.value[0]
-		} as LineOptions
-	)
-	chart.value.render()
-}
-
-onMounted(() => {
-	let data = props.list
-
+const processLocalList = () => {
+	let data = list
 	if (isGrouped.value || !isMultipleMeasure.value) {
 		localList.value = data
 	} else {
 		localList.value = fold(data, measures.value)
 	}
+}
 
-	nextTick(() => {
-		loadChart()
-	})
+const getOptions = (): LineOptions => {
+	let common = {} as Partial<Record<string, unknown>>
+	const isNotGroupedAndIsMultiple = !isGrouped.value && isMultipleMeasure.value
+	if (isNotGroupedAndIsMultiple) {
+		common.isGroup = true
+		common.seriesField = 'category'
+	} else if (isGrouped.value) {
+		common.isGroup = true
+		common.seriesField = columnName(dimensions.value[1])
+	}
+	return {
+		data: localList.value,
+		xField: columnName(dimensions.value[0]),
+		yField: isNotGroupedAndIsMultiple ? 'measure' : columnName(measures.value[0]),
+		...common,
+		...foundation.value,
+		lineStyle: {
+			lineWidth: settings.value.lineWidth
+		},
+		label: label({
+			formatter:
+				!settings.value.compactNumberLabel ?
+					null
+				:	(v: Partial<Record<string, string | number | Date>>) => {
+						const value = v['measure'] || v[columnName(firstMeasure.value)]
+						return formatValue(value, {
+							compactNumber: settings.value.compactNumberLabel
+						})
+					}
+		}),
+		smooth: settings.value.lineSmooth,
+		point:
+			settings.value.showPoint ?
+				{
+					shape: settings.value.pointType
+				}
+			:	null,
+		color:
+			isGrouped.value || isMultipleMeasure.value ? themeColors.value
+			: settings.value.showLegend ? themeColors.value
+			: themeColors.value[0]
+	}
+}
+
+const loadChart = () => {
+	if (!id.value) return
+	chart.value = new Line(id.value as HTMLElement, getOptions())
+	chart.value.render()
+}
+
+watch(
+	[dimensions, measures, list, task.settings],
+	() => {
+		processLocalList()
+		chart.value.update(getOptions())
+	},
+	{ deep: true }
+)
+
+onMounted(() => {
+	processLocalList()
+	nextTick(() => loadChart())
 })
 </script>
