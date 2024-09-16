a64b8284d23d4518b166247af37f38511bccb890
Sergio Carvalho <sergiopcn@gmail.com>
2024-09-16 14:24:16 -0300

    wip: start query screen

---
+++ src/views/studio/canvas/task-query/TaskQuery.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="task-query">
+		<DrawerView
+			:only-full-screen="true"
+			@close="$emit('close')"
+		>
+			<template #header>
+				<TaskQueryMenu @close="$emit('close')" />
+			</template>
+			<template #content>
+				<div class="bg-red-500 w-full">
+					<Splitpanes class="w-full">
+						<Pane
+							:size="viewControl.sideBarSize"
+							class="p-2 flex flex-col bg-blue-400"
+						>
+							<div class="bg-white">asd</div>
+							<!-- <TaskQuerySideBar /> -->
+						</Pane>
+						<Pane :size="viewControl.codeFrameSize">
+							codeFrame
+							<!-- <TaskStaticContentCodeFrame /> -->
+						</Pane>
+						<Pane :size="viewControl.previewSize">
+							preview
+							<!-- <TaskStaticContentCodeResult /> -->
+						</Pane>
+					</Splitpanes>
+				</div>
+			</template>
+		</DrawerView>
+	</div>
+</template>
+
+<script setup>
+import DrawerView from '@/components/drawer/DrawerView.vue'
+import TaskQueryMenu from '@/views/studio/canvas/task-query/components/TaskQueryMenu.vue'
+import TaskQuerySideBar from '@/views/studio/canvas/task-query/components/TaskQuerySideBar.vue'
+import { Pane, Splitpanes } from 'splitpanes'
+import { useCodeDataStore, useViewControlStore } from './store/useTaskQueryStore'
+
+defineEmits(['close'])
+const viewControl = useViewControlStore()
+</script>
---
+++ src/views/studio/canvas/task-query/components/TaskQuerySideBar.vue
@@ -0,0 +1,11 @@
+<template>
+	<aside class="flex-1 rounded bg-paper-100 dark:bg-carbon-200">
+		<h3>{{ $t('sources') }}</h3>
+		<div class="">asd</div>
+		<!-- <NScrollbar style="flex-1">asdas</NScrollbar> -->
+	</aside>
+</template>
+
+<script setup>
+import { NScrollbar } from 'naive-ui'
+</script>
