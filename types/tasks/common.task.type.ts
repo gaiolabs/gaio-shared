50ba2662cd622fa545aec0b9844a2505f560c351
Luis Vinicius <luisvinicius0906@gmail.com>
2024-09-25 14:57:49 -0300

    revert(Task): add properties back to CommonTaskType

--- types/tasks/common.task.type.ts
+++ types/tasks/common.task.type.ts
@@ -7,20 +7,20 @@ export type CommonTaskType = Partial<{
     repoId: string | null
     sourceType: 'source' | 'bucket'
     sourceId: string | null
-    // resultTable: string | null
-    // resultDatabase: string | null
-    // tableName: string | null
-    // databaseName: string | null
+    resultTable: string | null
+    resultDatabase: string | null
+    tableName: string | null
+    databaseName: string | null
     client: string
     position: PositionType
     type: string
-    // truncateTables: string[]
-    // dropTables: string[]
-    // schemaName: string | null
-    // clickhouse: boolean
-    // temporaryId: string
-    // shared: boolean
-    // created: boolean
-    // temporary: boolean
+    truncateTables: string[]
+    dropTables: string[]
+    schemaName: string | null
+    clickhouse: boolean
+    temporaryId: string
+    shared: boolean
+    created: boolean
+    temporary: boolean
     admin?: unknown
 }>
