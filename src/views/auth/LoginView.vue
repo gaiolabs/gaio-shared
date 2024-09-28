10f73da5c7ca063c9e212510f31bc308a6bb0e7d
sergiooak <sergio@gaio.io>
2024-09-28 17:50:16 -0300

    style: add pattern

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -4,13 +4,25 @@
 		class="flex relative flex-col realtive z-10 justify-between items-center h-full overflow-hidden"
 	>
 		<div
+			id="bg-dark"
 			:style="{ backgroundImage: `url(${bgDark})`, backgroundPosition: 'center center' }"
 			class="absolute bg-cover inset-0 bg z-[-1]"
 		></div>
 		<div
+			id="bg-light"
 			:style="{ backgroundImage: `url(${bg})`, backgroundPosition: 'center center' }"
 			class="absolute bg-cover inset-0 bg z-[-1] dark:opacity-0 opacity-100 !duration-[600ms] !transition-all"
 		></div>
+		<div
+			id="bg-pattern-wrapper"
+			class="absolute bottom-0 left-0 translate-x-[-52%] translate-y-[58%] absolute z-[-1]"
+		>
+			<div
+				id="bg-pattern"
+				:style="{ backgroundImage: `url(${pattern})` }"
+				class="dark:opacity-10 opacity-20 bg-cover w-[400vh] h-[400vh] bg"
+			></div>
+		</div>
 		<LoginHeaderComponent />
 		<main class="flex flex-col gap-6 lg:w-[28rem] pt-16">
 			<CompanyLogo />
@@ -39,6 +51,7 @@
 <script setup lang="ts">
 import bgDark from '@/assets/images/bg-dark.png'
 import bg from '@/assets/images/bg-light.png'
+import pattern from '@/assets/images/pattern.png'
 import { useAuthStore } from '@/stores'
 import { useDark } from '@vueuse/core'
 import { useI18n } from 'vue-i18n'
@@ -55,3 +68,18 @@ function doLogout() {
 	useAuthStore().logout()
 }
 </script>
+
+<style scoped>
+#bg-pattern {
+	animation: spin calc(15 * 60 * 1s) infinite;
+}
+
+@keyframes spin {
+	from {
+		transform: rotate(0deg);
+	}
+	to {
+		transform: rotate(360deg);
+	}
+}
+</style>
