e5f763b3a224fc12824dbba18c3d205b15308301
sergiooak <sergio@gaio.io>
2024-10-03 21:36:35 -0300

    feat: add mac detection

--- src/components/main-nav/LeftNav.vue
+++ src/components/main-nav/LeftNav.vue
@@ -7,12 +7,20 @@
 		/>
 
 		<button
-			class="g-base rounded-lg px-2 py-1 flex gap-4 items-center"
+			class="g-base rounded-lg px-2 py-1 flex gap-4 items-center relative group"
 			@click="useCommandKStore().show = true"
 		>
 			<IconComponent name="Search" />
 			<ul class="flex gap-1">
 				<kbd
+					v-if="os === 'mac'"
+					class="kbd-style"
+					:class="{ pressed: isMetaPressed }"
+				>
+					⌘ Cmd
+				</kbd>
+				<kbd
+					v-else
 					class="kbd-style"
 					:class="{ pressed: isCtrlPressed }"
 				>
@@ -25,6 +33,17 @@
 					K
 				</kbd>
 			</ul>
+
+			<span
+				class="absolute left-4 right-4 bottom-[-2px] h-[2px] dark:h-px bg-gradient-to-r from-sepia-500/0 via-sepia-400 to-sepia-500/0 dark:from-ochre-500/0 dark:via-ochre-400 dark:to-ochre-500/0 transition duration-150 opacity-0 scale-x-0 group-hover:opacity-100 group-hover:scale-x-100 z-[-1]"
+			></span>
+			<span
+				class="overflow-hidden absolute inset-0 transition origin-bottom duration-150 opacity-1 scale-0 group-hover:opacity-100 group-hover:scale-100 z-[-1]"
+			>
+				<span
+					class="absolute inset-x-4 -bottom-2 h-full bg-gradient-to-t dark:from-ochre-500/5 from-sepia-500/10 to-transparent blur rounded-t-full z-[-1]"
+				></span>
+			</span>
 		</button>
 	</nav>
 </template>
@@ -35,7 +54,19 @@ import { useDark, useMagicKeys } from '@vueuse/core'
 const isDark = useDark()
 const logoImage = computed(() => (isDark.value ? '/gaio-mini-dark.png' : '/gaio-mini-light.png'))
 
+const os = computed(() => {
+	const platform = navigator.userAgent
+	if (/Mac|iPad|iPhone|iPod/.test(platform)) {
+		return 'mac'
+	} else if (/Win/.test(platform)) {
+		return 'windows'
+	} else {
+		return 'other'
+	}
+})
+
 const keys = useMagicKeys()
+const isMetaPressed = keys.meta
 const isCtrlPressed = keys.ctrl
 const isKPressed = keys.k
 </script>
