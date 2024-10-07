6f4e4d46ce3849316267661ccad29e076e404e29
sergiooak <sergio@gaio.io>
2024-10-07 18:36:13 -0300

    feat(Icons): add Menu and More icon

---
+++ src/components/icons/MenuIcon.vue
@@ -0,0 +1,68 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 18 18"
+		height="18"
+		width="18"
+	>
+		<g :fill="fill">
+			<line
+				x1="2.25"
+				y1="9"
+				x2="15.75"
+				y2="9"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				:stroke-width="strokewidth"
+				:stroke="_secondaryfill"
+			></line>
+			<line
+				x1="2.25"
+				y1="3.75"
+				x2="15.75"
+				y2="3.75"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				:stroke-width="strokewidth"
+				:stroke="fill"
+			></line>
+			<line
+				x1="2.25"
+				y1="14.25"
+				x2="15.75"
+				y2="14.25"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				:stroke-width="strokewidth"
+				:stroke="fill"
+			></line>
+		</g>
+	</svg>
+</template>
+
+<script>
+export default {
+	props: {
+		strokewidth: {
+			type: [Number, String],
+			default: 1,
+		},
+		fill: {
+			type: String,
+			default: 'currentColor',
+		},
+		secondaryfill: {
+			type: String,
+			default: 'currentColor',
+		},
+	},
+	computed: {
+		_secondaryfill: function () {
+			return this.secondaryfill || this.fill
+		},
+	},
+}
+</script>
