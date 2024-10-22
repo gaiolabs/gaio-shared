38981ae937e71f7b4c6c48f0951e21b6bb5442f4
Luis Vinicius <luisvinicius0906@gmail.com>
2024-10-22 15:47:47 -0300

    feat(AutoML Task): split settings object into another type

--- types/tasks/automl.task.type.ts
+++ types/tasks/automl.task.type.ts
@@ -1,17 +1,19 @@
 import { type CommonTaskType } from './common.task.type'
 
+export type AutoMLTaskTypeSettings = {
+    seed: number
+    maxModels: number
+    projectId: string
+    splitFrame: number
+    projectName: string
+    reloadSource: boolean
+    removeColumns: Array<string>
+    trainingFrame: string
+    maxRuntimeSecs: number
+    responseColumn: string
+}
+
 export type AutoMLTaskType = Partial<{
-    settings: {
-        seed: number
-        maxModels: number
-        projectId: string
-        splitFrame: number
-        projectName: string
-        reloadSource: boolean
-        removeColumns: Array<string>
-        trainingFrame: string
-        maxRuntimeSecs: number
-        responseColumn: string
-    }
+    settings: AutoMLTaskTypeSettings
 }> &
     CommonTaskType
