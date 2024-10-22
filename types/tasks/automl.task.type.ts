fdabcdfa7c74aea5b0b93e9596c40504fc26caf1
Luis Vinicius <luisvinicius0906@gmail.com>
2024-10-22 15:21:21 -0300

    feat(AutoML Task): improve settings key from automl task type

--- types/tasks/automl.task.type.ts
+++ types/tasks/automl.task.type.ts
@@ -1,5 +1,15 @@
 import { type CommonTaskType } from './common.task.type'
 
 export type AutoMLTaskType = Partial<{
-  settings: unknown
-}> & CommonTaskType
+    seed: number
+    maxModels: number
+    projectId: string
+    splitFrame: number
+    projectName: string
+    reloadSource: boolean
+    removeColumns: Array<string>
+    trainingFrame: string
+    maxRuntimeSecs: number
+    responseColumn: string
+}> &
+    CommonTaskType
