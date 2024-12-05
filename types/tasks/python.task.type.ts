e45b2bc5b3da5ab55bc67f46a5d9870fcd24b3e2
Calebe Aires <calebeaires@gmail.com>
2024-12-05 12:35:28 -0500

    Update python.task.type.ts
    add id reference to env code snippet

--- types/tasks/python.task.type.ts
+++ types/tasks/python.task.type.ts
@@ -3,5 +3,6 @@ import { type CommonTaskType } from './common.task.type'
 export type PythonTaskType = Partial<{
     body: string
     codeSnippetId: string
+    envCodeSnippetId: string    
 }> &
     CommonTaskType
