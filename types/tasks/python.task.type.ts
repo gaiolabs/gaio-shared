26b4dfaa8cbf19a7d0486129943b10763008e643
Calebe Aires <calebeaires@gmail.com>
2024-12-13 15:49:50 -0500

    add code snippet props to Python Task

--- types/tasks/python.task.type.ts
+++ types/tasks/python.task.type.ts
@@ -3,6 +3,8 @@ import { type CommonTaskType } from './common.task.type'
 export type PythonTaskType = Partial<{
     body: string
     codeSnippetId: string
-    envCodeSnippetId: string    
+    codeSnippet: string
+    envCodeSnippetId: string
+    envCodeSnippet: string    
 }> &
     CommonTaskType
