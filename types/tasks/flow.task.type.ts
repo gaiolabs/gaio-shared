f960ed682c23e4f29b90df6b2553504178c676da
calebeaires <calebeaires@gmail.com>
2025-02-04 14:18:16 -0300

    else flow id

--- types/tasks/flow.task.type.ts
+++ types/tasks/flow.task.type.ts
@@ -4,6 +4,7 @@ import {ParamType} from "../core/param.type";
 export type FlowTaskType = Partial<{
     type: 'flow'
     flowId: string
+    elseFlowId: string
     loopSize: number
     runType: 'single' | 'conditional'
     params: ParamType[]
