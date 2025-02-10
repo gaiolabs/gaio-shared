eb11c2927b6e8fdd824cd49beb882780c39d0aed
calebeaires <calebeaires@gmail.com>
2025-02-10 12:08:25 -0300

    up: part 30

--- types/tasks/task.type.ts
+++ types/tasks/task.type.ts
@@ -143,9 +143,9 @@ export type TaskType =
     | CloudFileType
     | PcaTaskType
     | GenericType
-    // | ExternalOutputTaskType
-    // | ReportPreviewTaskType
-    // | FormCardType
+    | ExternalOutputTaskType
+    | ReportPreviewTaskType
+    | FormCardType
     | base
 
 export type CommonBuilderTaskType = base & TaskType
