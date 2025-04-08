22318f87683f9f30bacb6c1da4728566597849a7
Calebe Aires <calebeaires@gmail.com>
2025-04-08 17:02:41 -0300

    adjust task, export to file

--- types/tasks/export-to-file.task.type.ts
+++ types/tasks/export-to-file.task.type.ts
@@ -1,7 +1,7 @@
 import { type CommonTaskType } from './common.task.type'
 
 export type ExportToFileType = Partial<{
-    type: 'export'
+    type: 'exportToFile'
     separator: string
     compress: boolean
 }> &
