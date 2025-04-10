320cac85161fcffe7441b70a70ddf213d031cc08
Calebe Aires <calebeaires@gmail.com>
2025-04-10 13:00:38 -0300

    up api key type

--- types/core/api.type.ts
+++ types/core/api.type.ts
@@ -1,10 +1,22 @@
 import {GenericType} from "../generic.type";
 
+export type ApiKeyType = {
+    apiKeyId: string
+    apiKeyName: string
+    appId: string
+    secret: string
+    status: 'active' | 'inactive'
+    createdBy: string
+    createdAt: string
+    updateAt: string
+    updatedBy: string
+}
+
 export type ApiType = Partial<{
     apiId: string
     apiName: string
     description: string
-    token: string
+    apiKeyId: string
     flowId: string
     appId: string
     tableName: string
