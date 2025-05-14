5f6c078aee0b66d243144ba2bfa76fed51d250dc
EstevanPithan <estevanpithan@hotmail.com>
2025-05-14 16:40:38 -0300

    feat(maps): add MapsType definition and remove MapsTaskType
    - Create a new type definition for MapsType
    - Remove the MapsTaskType definition

---
+++ types/reports/maps.type.ts
@@ -0,0 +1,3 @@
+export type MapsType = Partial<{
+    mapId: string
+}> 
--- types/tasks/maps.task.type.ts
+++
@@ -1,7 +0,0 @@
-import type { CommonTaskType } from './common.task.type'
-
-export type MapsTaskType = Partial<{
-    type: 'maps'
-    reportType: 'maps'
-}> &
-    CommonTaskType
