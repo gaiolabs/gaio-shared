992c6d9972808cd24bbbf633113236a2547faa12
EstevanPithan <estevanpithan@hotmail.com>
2025-08-21 14:43:41 -0300

    feat(file-import): improve formatting for FileImportSymbols and FileImportTaskType
    - Standardize indentation for better readability
    - Maintain consistency with CommonTaskType structure
    
    feat(ftp-ingest): add FtpIngestTaskType definition
    
    - Define type for FTP ingest tasks with connection and storage properties

--- types/tasks/file-import.task.type.ts
+++ types/tasks/file-import.task.type.ts
@@ -2,38 +2,37 @@ import { FieldType } from '../core/field.type'
 import { type CommonTaskType } from './common.task.type'
 
 export type FileImportSymbols = {
-	comma: string
-	crNewLine: string
-	crlfNewLine: string
-	doubleQuotes: string
-	lfNewLine: string
-	newLine: string
-	none: string
-	pipe: string
-	semicolon: string
-	singleQuotes: string
-	tab: string
-	custom: string
+    comma: string
+    crNewLine: string
+    crlfNewLine: string
+    doubleQuotes: string
+    lfNewLine: string
+    newLine: string
+    none: string
+    pipe: string
+    semicolon: string
+    singleQuotes: string
+    tab: string
+    custom: string
 }
 
 export type FileImportTaskType = Partial<{
-	type: 'fileImport'
-	resultTable: string
-	from: string
-	transformType: string
-	append: boolean
-	splitFile: boolean
-	fixEncoding: string
-	fullPath: string
-	localPreparation: boolean
-	id: string
-	appId: string
-	fileName: string
-	fieldsTerminatedBy: keyof FileImportSymbols
-	fieldsTerminatedByCustom: string
-	columns: FieldType[]
-	transformFirst: boolean
-	databaseName: string
+    type: 'fileImport'
+    resultTable: string
+    from: string
+    transformType: string
+    append: boolean
+    splitFile: boolean
+    fixEncoding: string
+    fullPath: string
+    localPreparation: boolean
+    id: string
+    appId: string
+    fileName: string
+    fieldsTerminatedBy: keyof FileImportSymbols
+    fieldsTerminatedByCustom: string
+    columns: FieldType[]
+    transformFirst: boolean
+    databaseName: string
 }> &
-	CommonTaskType
-
+    CommonTaskType
