d38f8a55bb98708b62774baeacc3d7b0bb43343c
Calebe Aires <calebeaires@gmail.com>
2025-08-25 10:54:37 -0300

    Add tableInput and inputPosition fields to ApiType

--- types/tasks/rest.task.type.ts
+++ types/tasks/rest.task.type.ts
@@ -1,34 +1,34 @@
-import { type CommonTaskType } from './common.task.type'
+import type { CommonTaskType } from './common.task.type'
 import type { FieldType } from '../core/field.type'
 
 export type RestTaskType = Partial<{
-    resultTableTruncate: boolean
-    tableName: string
-    url: string
-    method: string
-    timeout: number
-    headers: { prop: string; value: string }[]
-    basicUsername: string
-    basicPassword: string
-    body: string
-    restType: string
-    createTableType: string
-    resultTable: string
-    resultTableFields: FieldType[]
-    dontFixBadData: boolean
-    keepLogTable: string
-    keepLogDays: number
-    resultTableProp: string
-    resultDataTypeProp: string
-    keepLogTableExtraColumn: string
-    transferSourceData: string[]
-    repoId: string
-    appId: string
-    id: string
-    isBatch: boolean
-    isBatchSchema: boolean
-    batchSize: number
-    bodySchema: string
-    type: 'rest'
+	resultTableTruncate: boolean
+	tableName: string
+	url: string
+	method: string
+	timeout: number
+	headers: { prop: string; value: string }[]
+	basicUsername: string
+	basicPassword: string
+	body: string
+	restType: string
+	createTableType: string
+	resultTable: string
+	resultTableFields: FieldType[]
+	dontFixBadData: boolean
+	keepLogTable: string
+	keepLogDays: number
+	resultTableProp: string
+	resultDataTypeProp: string
+	keepLogTableExtraColumn: string
+	transferSourceData: string[]
+	repoId: string
+	appId: string
+	id: string
+	isBatch: boolean
+	isBatchSchema: boolean
+	batchSize: number
+	bodySchema: string
+	type: 'rest'
 }> &
-    CommonTaskType
+	CommonTaskType
