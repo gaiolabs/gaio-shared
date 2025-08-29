10daa9e638057f5c7a65b5655cddc11e1b2f2935
calebeaires <calebeaires@gmail.com>
2025-08-29 16:14:32 -0300

    Removes GenericType from AiAgentTaskType
    Eliminates dependency on GenericType while developing the task

--- types/tasks/ai-agent.task.type.ts
+++ types/tasks/ai-agent.task.type.ts
@@ -1,10 +1,7 @@
-import type { GenericType } from '../generic.type'
 import type { CommonTaskType } from './common.task.type'
 
 export type AiAgentTaskType = {
-	id: string
-	label: string
 	type: 'aiAgent'
-} & CommonTaskType &
-	GenericType
+} & CommonTaskType
+
 // need to remove GenericType, defined here while the task is being build
