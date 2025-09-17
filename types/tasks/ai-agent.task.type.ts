30c34eb6547f73deb8805cb465f2729df199805f
calebeaires <calebeaires@gmail.com>
2025-09-17 14:14:09 -0300

    Adds result data type field to task type
    Introduces a field to specify the data type of AI agent task results for improved clarity and type safety.

--- types/tasks/ai-agent.task.type.ts
+++ types/tasks/ai-agent.task.type.ts
@@ -18,5 +18,6 @@ export type AiAgentTaskType = {
     insertMode: boolean
     aiManagerId: string
     resultType: 'param' | 'table'
+    resultDatType: 'String', 'JSON'
     resultReference: string
 } & CommonTaskType
