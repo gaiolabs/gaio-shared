bf2af129eb43468a9127db3a82143d08d6085f2c
calebeaires <calebeaires@gmail.com>
2025-10-22 14:52:40 -0300

    Updates agent and resource types, revises formatter config
    Adds new property to AI resource options type and streamlines Zed editor formatter settings for consistency.

--- types/core/agents.type.ts
+++ types/core/agents.type.ts
@@ -1,53 +1,56 @@
 import type { GenericType } from '../generic.type'
 
 export type AgentType = Partial<{
-	agentId: string
-	avatar: string
-	name: string
-	description: string
-	role: string
-	goal: string
-	backstory: string
-	options: string
-	settings: string
-	appId: string
-	createdAt: string // ISO string or Date object
-	updatedAt: string // ISO string or Date object
-	createdBy: string
-	updatedBy: string
+    agentId: string
+    avatar: string
+    name: string
+    description: string
+    role: string
+    goal: string
+    backstory: string
+    options: string
+    settings: string
+    appId: string
+    createdAt: string // ISO string or Date object
+    updatedAt: string // ISO string or Date object
+    createdBy: string
+    updatedBy: string
 }>
 
 export type AiResourceSettingsType = Partial<{
-	role: string
-	goal: string
-	backstory: string
-	avatar: string
+    role: string
+    goal: string
+    backstory: string
+    avatar: string
 }> &
-	GenericType
+    GenericType
 
 export type McpToolType = {
-		id: string
-		title: string
-		description: string
-		url: string
-		headers: Array<{ prop: string; value: string }>
+    id: string
+    title: string
+    description: string
+    url: string
+    headers: Array<{ prop: string; value: string }>
 }
 
 export type AiResourceType = Partial<{
-	aiResourceId: string
-	name: string
-	description: string
-	options: Partial<{
-	    diagram: string[]
-			discovery: string[]
-			mcpList: McpToolType[]
-	} & GenericType>
-	settings: AiResourceSettingsType
-	appId: string
-	shared: boolean
-	type: string
-	createdAt: string // ISO string or Date object
-	updatedAt: string // ISO string or Date object
-	createdBy: string
-	updatedBy: string
+    aiResourceId: string
+    name: string
+    description: string
+    options: Partial<
+        {
+            diagram: string[]
+            discovery: string[]
+            mcpList: McpToolType[]
+            mcpWikipedia: boolean
+        } & GenericType
+    >
+    settings: AiResourceSettingsType
+    appId: string
+    shared: boolean
+    type: string
+    createdAt: string // ISO string or Date object
+    updatedAt: string // ISO string or Date object
+    createdBy: string
+    updatedBy: string
 }>
