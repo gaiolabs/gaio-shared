a61653c04272ae83793e6fc536611cf5596e202d
Calebe Aires <calebeaires@gmail.com>
2025-12-02 11:05:53 -0300

    modifiy node ai agent task type

--- types/tasks/ai-agent.task.type.ts
+++ types/tasks/ai-agent.task.type.ts
@@ -3,21 +3,13 @@ import type { CommonTaskType } from './common.task.type'
 export type AiAgentTaskType = {
     type: 'aiAgent'
     prompt: string
-    toolOutputFormat: string
     outputFormat: string
     outputFormatEnabled: boolean
     systemPrompt: string
-    agentId: string
     retryOnFail: boolean
     retryMaxTries: number
     retryWaitBetweenTries: number
-    memoryType: string
-    memorySize: number
-    memorySessionId: string
-    memoryExpiration: number
-    toolOf: string
     insertMode: boolean
-    aiManagerId: string
     aiResourceId: string
     resultType: 'param' | 'table'
     resultDataType: 'String' | 'JSON'
