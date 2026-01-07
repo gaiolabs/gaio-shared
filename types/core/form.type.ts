76a4bed22b3f2e21f7c766cf69a6c7c612c180dc
calebeaires <calebeaires@gmail.com>
2026-01-07 12:02:43 -0300

    Adds formLoadFlowFinish property to form type
    Introduces a new boolean property for forms to handle flow load completion state.

--- types/core/form.type.ts
+++ types/core/form.type.ts
@@ -1,64 +1,65 @@
 export type FormElementsType = { cols: { [key: string]: FormFieldType[] }; id: string }
 
 export type FormType = Partial<{
-	formId: string
-	flowId: string
-	fieldSize: string
-	formLabel: string
-	formName: string
-	formDescription: string
-	formOnError: string
-	formOnSuccess: string
-	formType: string // could be further refined to 'loadFlow' | 'callFlow'
-	formReload: boolean
-	formConfirm: boolean
-	formConfirmTitle: string
-	formConfirmDescription: string
-	formClearOnStart: boolean
-	formResetParams: boolean
-	formForceDefault: boolean
-	formElements: FormElementsType[]
+    formId: string
+    flowId: string
+    fieldSize: string
+    formLabel: string
+    formName: string
+    formDescription: string
+    formOnError: string
+    formOnSuccess: string
+    formType: string // could be further refined to 'loadFlow' | 'callFlow'
+    formLoadFlowFinish: boolean
+    formReload: boolean
+    formConfirm: boolean
+    formConfirmTitle: string
+    formConfirmDescription: string
+    formClearOnStart: boolean
+    formResetParams: boolean
+    formForceDefault: boolean
+    formElements: FormElementsType[]
 }>
 
 export type FormFieldType = Partial<{
-	id: string
-	label: string
-	type: string
-	source: string
-	required: boolean
-	isButton: boolean
-	hidden: boolean
-	value: string | number | string[] | number[] // since it's an array with empty elements, it should be any[].
-	extraValue: string | number | string[] | number[] // since it's an array with empty elements, it should be any[].
-	list: { value: string; label: string; default?: boolean }[]
-	placeholder: string
-	description: string
-	min: number | undefined
-	max: number | undefined
-	step: number | undefined
-	paramName: string
-	textareaRows: number
-	labelFormat: string
-	fieldSize: string
-	readonly: boolean
-	longText: boolean
-	extraParamName: string
-	all: boolean
-	multiple: boolean
-	datePickerType: string
-	choices: any[] // assuming choices can be of any type
-	valueIsNumber: boolean
-	isVertical: boolean
-	isRange: boolean
-	sliderType: string
-	sliderShowStops: boolean
-	sliderMinValue: number
-	sliderMaxValue: number
-	sliderStepValue: number
-	bucketDatabase: string
-	bucketTable: string
-	bucketFieldLabel: string
-	bucketFieldLimit: number
-	bucketFieldValue: string
-	bucketFieldOrder: 'asc' | 'desc' | undefined
+    id: string
+    label: string
+    type: string
+    source: string
+    required: boolean
+    isButton: boolean
+    hidden: boolean
+    value: string | number | string[] | number[] // since it's an array with empty elements, it should be any[].
+    extraValue: string | number | string[] | number[] // since it's an array with empty elements, it should be any[].
+    list: { value: string; label: string; default?: boolean }[]
+    placeholder: string
+    description: string
+    min: number | undefined
+    max: number | undefined
+    step: number | undefined
+    paramName: string
+    textareaRows: number
+    labelFormat: string
+    fieldSize: string
+    readonly: boolean
+    longText: boolean
+    extraParamName: string
+    all: boolean
+    multiple: boolean
+    datePickerType: string
+    choices: any[] // assuming choices can be of any type
+    valueIsNumber: boolean
+    isVertical: boolean
+    isRange: boolean
+    sliderType: string
+    sliderShowStops: boolean
+    sliderMinValue: number
+    sliderMaxValue: number
+    sliderStepValue: number
+    bucketDatabase: string
+    bucketTable: string
+    bucketFieldLabel: string
+    bucketFieldLimit: number
+    bucketFieldValue: string
+    bucketFieldOrder: 'asc' | 'desc' | undefined
 }>
